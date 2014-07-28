d4acf4e24a982e912908acab5a797dd5bb004761
Chris Peterson <chris@cpeterson.org>
2014-07-27 23:01:12 -0400

    scraper works, storage doesn't (yet)

--- storeBlogs.py
+++ storeBlogs.py
@@ -14,7 +14,7 @@ now = datetime.fromtimestamp(time.time()).strftime('as of %b %d %Y at %H_%M')
 
 #open database connection
 def initializeDatabase():
-	conn = sqlite3.connect('../DATADUMP/blogData ' + now + '.db')
+	conn = sqlite3.connect('../DATADUMP/blogdb/blogData ' + now + '.db')
 	db = conn.cursor()
 
 	#create table for entry metadata
@@ -26,25 +26,25 @@ def initializeDatabase():
 				 	 wordcount int)''')
 
 	#create table for entry content
-	db.execute('''CREATE TABLE entry_content (entry_author text, entry_title text,
+	db.execute('''CREATE TABLE IF NOT EXISTS entry_content (entry_author text, entry_title text,
 				 	 entry_date date, entry_stamp timestamp, entry_delta int, 
 				 	 entry_link text unique, entry_text text)''')
 
 	#create table for comments
-	db.execute('''CREATE TABLE entry_comments (entry_author text, entry_title text,
+	db.execute('''CREATE TABLE IF NOT EXISTS entry_comments (entry_author text, entry_title text,
 				 	 entry_date date, entry_stamp timestamp, entry_delta int, 
 				 	 entry_link text unique, commenter text, comment_date text, comment_stamp timestamp,
 				 	 comment_text text, comment_system text, comment_sentiment int,
-				 	 comment_num text, )''')
+				 	 comment_num int)''')
 
 	#create table for entities mentioned
-	db.execute('''CREATE TABLE entities (entry_author text, entry_title text,
+	db.execute('''CREATE TABLE IF NOT EXISTS entities (entry_author text, entry_title text,
 				 	 entry_date date, entry_stamp timestamp, entry_delta int, 
 				 	 entry_link text unique, entityCount int, entityName text, 
 				 	 entityType text)''')
 
 	#create table for places mentioned
-	db.execute('''CREATE TABLE places (entry_author text, entry_title text,
+	db.execute('''CREATE TABLE IF NOT EXISTS places (entry_author text, entry_title text,
 				 	 entry_date date, entry_stamp timestamp, entry_delta int, 
 				 	 entry_link text unique, placeName text, countryCode text, 
 				 	 latitude int, longitude int, population int)''')
@@ -54,7 +54,7 @@ def insertMetaData(entryMeta):
 	conn = sqlite3.connect('../DATADUMP/blogData ' + now + '.db')
 	db = conn.cursor()
 	keys = ', '.join(entryMeta.keys())
-	db.execute('INSERT INTO metadata VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+	db.execute('INSERT INTO entry_metadata VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
           (keys))
 	conn.commit()
 
